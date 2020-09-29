4e83f45c45818662fefc338f381ddfbdaf3d7c13
Elbert Castañeda <elbert@sbxretail.com>
2020-09-28 22:54:55 -0500

    Update Rolls program

--- src/main/java/algs_four_practices/data_abstraction/Rolls.java
+++ src/main/java/algs_four_practices/data_abstraction/Rolls.java
@@ -10,36 +10,35 @@ import edu.princeton.cs.algs4.StdRandom;
  *
  *  Simulate the rolling of n 6-sided dice.
  *
- *  % java Rolls 1000000
- *  167308 1s
- *  166540 2s
- *  166087 3s
- *  167051 4s
- *  166422 5s
- *  166592 6s
+ *  % java Rolls 6 1000000
+ *  167308 0s
+ *  166540 1s
+ *  166087 2s
+ *  167051 3s
+ *  166422 4s
+ *  166592 5s
  *
  ******************************************************************************/
 
 public class Rolls {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        int SIDES = 6;
+        int trials = Integer.parseInt(args[1]);
 
-        // initialize counters
-        Counter[] rolls = new Counter[SIDES+1];
-        for (int i = 1; i <= SIDES; i++) {
-            rolls[i] = new Counter(i + "s");
+        // create n counters
+        Counter[] hits = new Counter[n];
+        for (int i = 0; i < n; i++) {
+            hits[i] = new Counter(i + "s");
         }
 
-        // flip dice
-        for (int j = 0; j < n; j++) {
-            int result = StdRandom.uniform(1, SIDES+1);
-            rolls[result].increment();
+        // increment trials counters at random
+        for (int t = 0; t < trials; t++) {
+            hits[StdRandom.uniform(n)].increment();
         }
 
         // print results
-        for (int i = 1; i <= SIDES; i++) {
-            StdOut.println(rolls[i]);
+        for (int i = 0; i < n; i++) {
+            StdOut.println(hits[i]);
         }
     }
 }
